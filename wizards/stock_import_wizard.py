1588d6b2756824f67e488c2f896c5c8613df68e9
Diego Gajardo <deskodoo@a2group.cl>
2024-08-08 12:09:02 -0400

    line ids

--- wizards/stock_import_wizard.py
+++ wizards/stock_import_wizard.py
@@ -18,12 +18,6 @@ class StockImportWizard(models.TransientModel):
         book = xlrd.open_workbook(file_contents=file_data)
         sheet = book.sheet_by_index(0)
 
-        priority_map = {
-            'Low': '0',
-            'Normal': '1',
-            'High': '2'
-        }
-
         for rowx in range(1, sheet.nrows):  # Comienza desde la fila 2
             row = sheet.row(rowx)
             
@@ -33,16 +27,19 @@ class StockImportWizard(models.TransientModel):
                 'scheduled_date': row[2].value,
                 'origin': row[10].value,
                 'priority': '1',  # Asumiendo prioridad normal si no hay campo en el Excel
-                'move_lines': [(0, 0, {
-                    'product_id': self._get_product_id(row[3].value),
-                    'product_uom_qty': float(row[13].value),
-                    'product_uom': self._get_uom_id(row[4].value),
-                    'location_id': self._get_location_id(row[0].value),
-                    'location_dest_id': self._get_location_id(row[1].value),
-                    'name': row[3].value,
-                })]
             }
-            self.env['stock.picking'].create(picking_data)
+            picking = self.env['stock.picking'].create(picking_data)
+            
+            move_data = {
+                'product_id': self._get_product_id(row[3].value),
+                'product_uom_qty': float(row[13].value),
+                'product_uom': self._get_uom_id(row[4].value),
+                'picking_id': picking.id,
+                'location_id': self._get_location_id(row[0].value),
+                'location_dest_id': self._get_location_id(row[1].value),
+                'name': row[3].value,
+            }
+            self.env['stock.move'].create(move_data)
 
     def _get_location_id(self, name):
         location = self.env['stock.location'].search([('name', '=', name)], limit=1)
@@ -51,7 +48,8 @@ class StockImportWizard(models.TransientModel):
         return location.id
 
     def _get_product_id(self, name):
-        product = self.env['product.product'].search([('default_code', '=', name.split('] ')[1].split(' ')[0])], limit=1)
+        product_code = name.split('] ')[1].split(' ')[0]
+        product = self.env['product.product'].search([('default_code', '=', product_code)], limit=1)
         if not product:
             product = self.env['product.product'].create({'name': name})
         return product.id
